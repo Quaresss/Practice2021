eff22a87f99799978efc044edca25d07b92b42b6
Quaresss <roktarik777@gmail.com>
2021-08-02 03:10:36 +0400

    "main"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,8 @@
       <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
           <h1 class="fw-light">Добро пожаловать.</h1>
+          
+
           <p>
             <a href="news" class="btn btn-primary my-2">Читать новости</a>
             <a href="#" class="btn btn-secondary my-2">Предложить новость</a>
--- resources/views/show-post.blade.php
+++ resources/views/show-post.blade.php
@@ -1,18 +1,25 @@
 @extends('layout')
-@section('title')@post->title @endsection
+@section('title')Новость@endsection
 @section('main.content')
  
-    <div class="btn-group mb-4" role="group" aria-label="Basic outlined example">
+    <div class="btn-group mb-4" role="group" aria-label="Basic outlined example"> 
         @foreach ($categories as $category) 
-        <a href="{{route('getPostsByCategory',$category->slug)}}" class="btn btn-outline-primary">{{$category->title}}</a>
+        <a href="{{route('getPostsByCategory',$category->slug)}}"class="btn btn-primary">{{$category->title}} </a>
         @endforeach
     </div>   
- <div><a href="/news" class="btn btn-outline-primary mb-4">Вернуться ко всем новостям</a></div>   
-<div><a href="{{route('getPostsByCategory', $slug_category)}}" class="btn btn-outline-primary mb-4">Вернуться к категории</a></div>
+ <div><a href="/news" class="btn btn-primary mb-4">Вернуться ко всем новостям</a></div>   
+<div><a href="{{route('getPostsByCategory', $slug_category)}}" class="btn btn-primary mb-4">Вернуться к категории</a></div>
 
-<article>
-    {!!$post->text!!}
- </article>
-     
+ 
+<div class="card mb-4">
+    
+ <div class="card-body">
+    <h2>
+        <article>
+            {!!$post->text!!}  
+        </article>
+    </h2>    
+ </div>
+</div>
 
 @endsection
