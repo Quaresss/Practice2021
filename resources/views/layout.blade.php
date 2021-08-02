2e114732c97e5059f0b2efdb851a28a2221b374f
Quaresss <86346406+Quaresss@users.noreply.github.com>
2021-08-02 12:30:50 +0400

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css">
 </head>
-<body background="https://img1.akspic.ru/originals/9/0/5/1/5/151509-zdanie-chernyj_belyj_m-angle-dnevnoe_vremya-chernyj_i_belyj-1920x1080.jpg">
+<body background="https://img1.akspic.ru/originals/9/0/5/1/5/151509-zdanie-chernyj_belyj_m-angle-dnevnoe_vremya-chernyj_i_belyj-1920x1080.jpg" style="background-size: cover">
 <div style="height: 45px;background-image:url('{{ asset('images/image.jpg') }}');border-radius: 5px;position:relative">
 <div class="container">
   <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
